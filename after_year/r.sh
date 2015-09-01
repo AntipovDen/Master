f1d4b749ebc7b8fe84a211de47aa2152274e67e3
AntipovDen <antipovden@yandex.ru>
2015-09-01 13:44:11 +0300

    new r.sh

--- after_year/r.sh
+++ after_year/r.sh
@@ -1,1 +1,10 @@
-pdflatex article.tex
+if [ "$1" == "" ]; then
+  pdflatex article.tex  
+elif [ "$1" == "clear" ]; then
+  rm *.log
+  rm *.aux
+  rm *.pdf
+  rm *.backup
+else
+  echo "usage: ./r.sh [clear]"
+fi
