724b513fa758d0b7498a5c444fbb14405c089b79
AntipovDen <antipovden@yandex.ru>
2015-09-04 17:06:39 +0300

    clion integrated with git

--- dinprog/main.cpp
+++ dinprog/main.cpp
@@ -4,7 +4,17 @@
 
 int main()
 {
-
+    //calculate combination n of k for n = 0..400, k = 0..n
+    double** comb = new double*[401];
+    for (int i = 0; i <= 400; i++)
+    {
+        comb[i] = new double[i + 1];
+        comb[i][0] = comb[i][i] = 1;
+        for (int j = 1; j < i; j++)
+        {
+            comb[i][j] = comb[i][j - 1] * (i - j + 1) / j;
+        }
+    }
 
     return 0;
 }
