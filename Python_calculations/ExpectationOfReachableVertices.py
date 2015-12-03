b873ae628f7bc8730baed8011c4ee3ad01c5fc94
AntipovDen <antipovden@yandex.ru>
2015-12-03 19:54:46 +0300

    tries to find the formula for the expectations

--- Python_calculations/ExpectationOfReachableVertices.py
+++ Python_calculations/ExpectationOfReachableVertices.py
@@ -1,14 +1,15 @@
 __author__ = 'dantipov'
 
 from random import randint
-# from matplotlib import pyplot as plt
-
+from matplotlib import pyplot as plt
+from scipy.special import gammainc
+from math import log
 
 def expectations(vertices, max_edges):
     expect = [1] + [0] * (max_edges - 1)
     for edges in range(1, max_edges):
         prev = expect[edges - 1]
-        expect[edges] = prev + (prev * (vertices - prev) / vertices) ** 2 / (vertices + edges)
+        expect[edges] = prev + (prev * (vertices - prev) / (vertices ** 1.5 + edges))  #/ (vertices)
     return expect
 
 
@@ -29,27 +30,30 @@ def modelling_run(vertices, edges, runs):
 
 
 # just experiments
-with open('data/reachable_vertices.out', 'w') as f:
-    runs = 5000
-    for vertices in range(5, 6):
-        f.write(" ".join([str(modelling_run(vertices, edges, runs)) for edges in range(vertices ** 2 + 1)]))
-        f.write('\n')
-        f.flush()
-exit(0)
+# with open('data/reachable_vertices.out', 'w') as f:
+#     runs = 5000
+#     for vertices in range(5, 6):
+#         f.write(" ".join([str(modelling_run(vertices, edges, runs)) for edges in range(vertices ** 2 + 1)]))
+#         f.write('\n')
+#         f.flush()
+# exit(0)
 
-# vertices = 20
-# max_edges = 150
-# edges = [e for e in range(max_edges)]
-# runs = 100
-#
-# expect = expectations(vertices, max_edges)
-#
-# experiment = [modelling_run(vertices, e, runs) for e in edges]
-#with open('data/reachable_vertices.out', 'r') as f:
-#    experiment = [float(i) for i in f.readline().split()]
+vertices = 20
+max_edges = 401
+edges = [e for e in range(max_edges)]
 
-#addendnum = [experiment[i] * (vertices - experiment[i]) / (experiment[i + 1] - experiment[i]) for i in edges[:-1]]
+expect = expectations(vertices, max_edges)
+
+with open('data/reachable_vertices20.out', 'r') as f:
+   experiment = [float(i) for i in f.readlines()[-1].split()]
+
+# addendnum = [experiment[i] * (vertices - experiment[i]) / (experiment[i + 1] - experiment[i]) for i in edges[:-1]]
+
+difference = [experiment[i + 1] - experiment[i] for i in range(max_edges - 1)]
+expected_defferences = [expect[i + 1] - expect[i] for i in range(max_edges - 1)]
 
 
-# plt.plot(edges, expect, 'bo', edges, experiment, 'ro')
-# plt.show()
+
+plt.plot(experiment[:-1], difference, 'ro', expect[:-1], expected_defferences, 'bo')
+#plt.plot(edges, expect, 'bo', edges, experiment[:max_edges], 'ro')
+plt.show()
