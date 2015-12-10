6813af523d381b211eaa7d5968e383ca47c021a7
AntipovDen <antipovden@yandex.ru>
2015-12-10 19:31:58 +0300

    hm... maybe i should do smth with that assumption? or create another one?

--- Python_calculations/ExpectationOfReachableVertices.py
+++ Python_calculations/ExpectationOfReachableVertices.py
@@ -5,55 +5,69 @@ from matplotlib import pyplot as plt
 from scipy.special import gammainc
 from math import log
 
-def expectations(vertices, max_edges):
+
+def get_expectations(vertices, max_edges):
     expect = [1] + [0] * (max_edges - 1)
     for edges in range(1, max_edges):
         prev = expect[edges - 1]
-        expect[edges] = prev + (prev * (vertices - prev) / (vertices ** 1.5 + edges))  #/ (vertices)
+        expect[edges] = prev + (prev * (vertices - prev) / vertices) ** 2 / vertices
     return expect
 
 
-def model_graph(vertices, edges):
-    reachable = [{v} for v in range(vertices)]
-    for _ in range(edges):
-        v_from, v_to = randint(0, vertices - 1), randint(0, vertices - 1)
-        reachable[v_from] = reachable[v_from].union(reachable[v_to])
-        for v in range(vertices):
-            if v_from in reachable[v]:
-                reachable[v] = reachable[v].union(reachable[v_to])
-    return sum([len(reachable[i]) for i in range(vertices)]) / vertices
+def get_experiment(filename, max_edges):
+    with open(filename, 'r') as f:
+        return [float(i) for i in f.readline().split()[:max_edges]]
 
 
-def modelling_run(vertices, edges, runs):
-    print("modelling for", vertices, "vertices and", edges, "edges")
-    return sum([model_graph(vertices, edges) for _ in range(runs)]) / runs
+def get_differences(distribution):
+    return [distribution[i + 1] - distribution[i] for i in range(len(distribution) - 1)]
 
 
-# just experiments
-# with open('data/reachable_vertices.out', 'w') as f:
-#     runs = 5000
-#     for vertices in range(5, 6):
-#         f.write(" ".join([str(modelling_run(vertices, edges, runs)) for edges in range(vertices ** 2 + 1)]))
-#         f.write('\n')
-#         f.flush()
-# exit(0)
+# this is how experiments were done
+
+# def model_graph(vertices, edges):
+#     reachable = [{v} for v in range(vertices)]
+#     for _ in range(edges):
+#         v_from, v_to = randint(0, vertices - 1), randint(0, vertices - 1)
+#         reachable[v_from] = reachable[v_from].union(reachable[v_to])
+#         for v in range(vertices):
+#             if v_from in reachable[v]:
+#                 reachable[v] = reachable[v].union(reachable[v_to])
+#     return sum([len(reachable[i]) for i in range(vertices)]) / vertices
+#
+#
+# def modelling_run(vertices, edges, runs):
+#     print("modelling for", vertices, "vertices and", edges, "edges")
+#     return sum([model_graph(vertices, edges) for _ in range(runs)]) / runs
+
 
 vertices = 20
-max_edges = 401
+max_edges = 150
 edges = [e for e in range(max_edges)]
 
-expect = expectations(vertices, max_edges)
+expect = get_expectations(vertices, max_edges)
+experiment = get_experiment('data/reachable_vertices20.out', max_edges)
 
-with open('data/reachable_vertices20.out', 'r') as f:
-   experiment = [float(i) for i in f.readlines()[-1].split()]
-
-# addendnum = [experiment[i] * (vertices - experiment[i]) / (experiment[i + 1] - experiment[i]) for i in edges[:-1]]
-
-difference = [experiment[i + 1] - experiment[i] for i in range(max_edges - 1)]
-expected_defferences = [expect[i + 1] - expect[i] for i in range(max_edges - 1)]
+expected_dif = get_differences(expect)
+experiment_dif = get_differences(experiment)
 
 
+# # show experiment and expected distributions
+# plt.plot(edges, experiment, 'ro', edges, expect, 'bo')
+# plt.show()
+#
+# # show differences depending of number of edges in graph
+# plt.plot(edges[:-1], experiment_dif, 'ro', edges[:-1], expected_dif, 'bo')
+# plt.show()
+#
+#
+# # show differences depending of number of number of reachable vertices
+# plt.plot(experiment[:-1], experiment_dif, 'ro', expect[:-1], expected_dif, 'bo')
+# plt.show()
 
-plt.plot(experiment[:-1], difference, 'ro', expect[:-1], expected_defferences, 'bo')
-#plt.plot(edges, expect, 'bo', edges, experiment[:max_edges], 'ro')
+# assumption: expected_dif[i] = experiment[i] * (vertices - experiment[i]) * smth. let's find smth!
+assumption = [experiment_dif[i] / (experiment[i] * (vertices - experiment[i])) for i in range(max_edges - 1)]
+plt.plot(edges[:-1], assumption, 'ro') #, edges[:-1], expected_dif, 'bo')
+plt.show()
+plt.plot(experiment[:-1], assumption, 'ro')#, expect[:-1], expected_dif, 'bo')
 plt.show()
