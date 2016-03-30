e920916f6e12d12706e39030cb78522ca389f9fa
AntipovDen <antipovden@yandex.ru>
2016-03-30 18:46:03 +0300

    think how to check changes in order

--- Python_calculations/DijkstraAlgorithm.py
+++ Python_calculations/DijkstraAlgorithm.py
@@ -4,7 +4,7 @@ __author__ = 'Den'
 
 # from math import sqrt, ceil
 # from matplotlib import pyplot as plt
-from random import randrange
+from random import randrange, random
 
 
 # Graph edge class
@@ -95,18 +95,18 @@ def dijkstra(v, edges):
     vertices = [Vertex(0., 0)] + [Vertex(float('inf'), i) for i in range(1, v)]
     left_vertices = {i for i in range(1, v)}
 
-    relaxed = set()
+    relaxed = 0
 
     cur_vertex = vertices[0]
     answer = []
-    while True :
+    while True:
         graph[cur_vertex.number].sort()
         for edge in graph[cur_vertex.number]:
             relaxValue = cur_vertex.mark + edge.weight
 
             if vertices[edge.to].mark > relaxValue:
                 vertices[edge.to].mark = relaxValue
-                relaxed.add((cur_vertex.number, edge.to, edge.weight))
+                relaxed += 1  # .add((cur_vertex.number, edge.to, edge.weight))
 
         answer.append(cur_vertex)
         if len(left_vertices) > 0:
@@ -121,104 +121,47 @@ def dijkstra(v, edges):
 
 
 # operations for EA
-def init_graph(v, e, max_weight,):
-    return [(randrange(v), randrange(v), randrange(max_weight)) for _ in range(e)]
+def init_graph(v, e):
+    return [(randrange(v), randrange(v), random()) for _ in range(e)]
 
 
-def mutate(v, e, max_weight):
-    e[randrange(len(e))] = (randrange(v), randrange(v), randrange(max_weight))
+def mutate(v, e):
+    e[randrange(len(e))] = (randrange(v), randrange(v), random())
 
-
-# function take statistics about the graph:
-# 1) number of vertices that are reachable from the start vertex
-# 2) number of edges that couldn't be reached from the start vertex
-#    but were mutated before
-# 3) number of leaves
-# 4) the max depth  in the graph
-def statistics(v, edges, maxWeight):  #TODO: check the current statistics and modify them
-    graph = [[] for i in range(v)]
-
-    for edge in edges:
-        graph[edge[0]].append(Edge(edge[1], edge[2]))
-
-    depthes = [0] + [v] * (v - 1)
-    bfsQueue = [0]
-
-    while len(bfsQueue) > 0:
-        i = bfsQueue.pop(0)
-        for edge in graph[i]:
-            j = edge.to
-            if depthes[j] > depthes[i] + 1:
-                depthes[j] = depthes[i] + 1
-                bfsQueue.append(j)
-
-    reachable = 0
-    depth = 0
-    for i in range(v):
-        if depthes[i] < v:
-            reachable += 1
-            if depthes[i] >= depth:
-                depth = depthes[i]
-
-    unreachableEdges = 0 # all edges - edges 1->0 - edges, reached by bfs
-    leaves = 0 #number of vertices with only one input edge and no output edges
-
-    inputEdges = [0] * v
-    outputEdges = [0] * v
-    for edge in edges:
-        inputEdges[edge[1]] += 1
-        outputEdges[edge[0]] += 2
-        if (depthes[edge[0]] == v and (edge[0] != 1 or edge[1] != 0 or edge[2] != maxWeight)):
-            unreachableEdges += 1
-    for i in range(v):
-        if (inputEdges[i] == 1 and outputEdges[i] == 0 and depthes[i] != v):
-            leaves += 1
-
-    return [reachable, unreachableEdges, leaves, depth]
 
 
 # EA run
 # now it opimizes only with mutating edgges that are not in the connected components into the edges
 # that lead from connected component out of it
-def evo_run(vertices, edges, max_weight):
+def evo_run(vertices, edges):
     global logfile, run_number
     run_number += 1
     logfile.write("Run number {}\n".format(run_number))
     logfile.flush()
-    if vertices > 2 * edges:
-        algorithm = dijkstra_many_vertices
-    else:
-        algorithm = dijkstra
-    graph = init_graph(vertices, edges, max_weight)
+    # if vertices > 2 * edges:
+    #     algorithm = dijkstra_many_vertices
+    # else:
+    algorithm = dijkstra
+    graph = init_graph(vertices, edges)
     f = 0
     iterations = 0
-    p_relax = []
+    results = []
     while f != edges:
         nextGen = graph.copy()
-        mutate(vertices, nextGen, max_weight)
+        mutate(vertices, nextGen)
         relaxed, answer = algorithm(vertices, nextGen)
 
-        if len(relaxed) >= f:
-            if len(relaxed) > f:
-                logfile.write("relaxed: {} with {} iterations\n".format(len(relaxed), iterations))
+        if relaxed >= f:
+            if relaxed > f:
+                logfile.write("relaxed: {} with {} iterations\n".format(relaxed, iterations))
                 logfile.flush()
-            p_relax.append(get_p_relax(answer, relaxed, max_weight))
+            if len(results) == 0 or results[-1][1] != answer:
+                results.append((iterations, relaxed, answer))
             graph = nextGen
             f = relaxed
         iterations += 1
 
-    return iterations
-
-
-def get_p_relax(answer, relaxed, wm):
-    position = {answer[i].number : i for i in range(len(answer))}
-    edges = [[] for _ in range(len(answer))]
-    for edge in relaxed:
-        edges[position[edge[1]]].append((edge[0], edge[2]))
-    for e in edges:
-        e.sort()
-        e.reverse()
-    #TODO: understatand how to get this probability
+    return results
 
 
 def optimize_simple(vertices, edges, max_weight):
@@ -229,12 +172,8 @@ def optimize_simple(vertices, edges, max_weight):
     f = 0
     iterations = 0
     current_connected_component = {0}
-    # no_change_iterations = 0
     while f != edges:
-        # if no_change_iterations > 1000000:
-        #     exit(0)
         iterations += 1
-        # nextGen = graph.copy()
 
         # mutation with lower probability
         edge_number = randrange(edges)
@@ -244,9 +183,6 @@ def optimize_simple(vertices, edges, max_weight):
             graph[edge_number] = (v_from, v_to, max_weight)
             current_connected_component.add(v_to)
             f += 1
-        #     no_change_iterations = 0
-        # else:
-        #     no_change_iterations += 1
 
     return iterations
 
@@ -254,19 +190,26 @@ if len(sys.argv) == 1:
     stream_number = ''
 else:
     stream_number = '_' + sys.argv[1]
-logfile = open("data/logs/dijkstra_edges_equal_vertices{}.log".format(stream_number), 'w')
+logfile = open("data/logs/dijkstra_distance_dynamics{}.log".format(stream_number), 'w')
 runs = 10
-with open('data/dijkstra_edges_equal_vertices{}.out'.format(stream_number), 'w') as f:
-    for e in [100, 500, 1000]:
-        for vertices in [e, e + 1, e + 2, e + 5, e + 10, e + 20]:
-            for max_weight in [2, e // 2, e, e + 20, 2 * e]: # range(edges + 10, 10 * edges, 10):
-                run_number = 0
-                logfile.write("V{}E{}W{}\n".format(vertices, e, max_weight))
-                logfile.flush()
-                res = sum([evo_run(vertices, e, max_weight) for _ in range(runs)]) / runs
-                f.write(str(res) + ' ')
-                f.flush()
-            f.write('\n')
+run_number = 0
+with open('data/dijkstra_distance_dynamics{}.out'.format(stream_number), 'w') as f:
+    e = 150
+    v = 30
+    for _ in range(runs):
+        results = evo_run(v, e)
+        for i in range(1, len(results)):
+            iterations, fitness, cur_res = results[i]
+            prev_res = results[i-1][2]
+            if len(cur_res) != len(prev_res):
+                if len(cur_res) > len(prev_res):
+                    res = '+{}'.format(len(cur_res) - len(prev_res))
+                else:
+                    res = str(len(cur_res) - len(prev_res))
+                print(res)
+            else:
+                res = sum([abs(results[i][1][j] - results[i-1][1][j]) for j in range(len(results[i][1]))])
+            f.write("iterations: {} fitness: {} res: {}\n".format(str(results[i][0]).ljust(6), str(results[i][2]).ljust(2), res))
             f.flush()
 logfile.close()
 
